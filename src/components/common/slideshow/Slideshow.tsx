4f5d063fc327daf9149dff532aa08ddc50de43c3
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2024-09-04 22:35:11 -0400

    Update Slideshow.tsx

--- src/components/common/slideshow/Slideshow.tsx
+++ src/components/common/slideshow/Slideshow.tsx
@@ -32,7 +32,7 @@ const Slideshow: React.FC<SlideshowProps> = ({
     const { lang } = useGlobalData();
     const [translate, setTranslate] = useState<number>(0);
     const [currentIndex, setCurrentIndex] = useState<number>(0);
-    const [infoArr, setInfoArr] = useState<Slide[]>([...slides]);
+    const [infoArr, setInfoArr] = useState<Slide[]>([]);
 
     useEffect(() => {
         setInfoArr([...slides]);
@@ -59,7 +59,7 @@ const Slideshow: React.FC<SlideshowProps> = ({
                     {slides?.map((slide, i) => {
                         return (
                             <Circle
-                                key={slide.id}
+                                key={`${slide?.img}_1`}
                                 $selected={i === currentIndex}
                             />
                         );
@@ -67,35 +67,37 @@ const Slideshow: React.FC<SlideshowProps> = ({
                 </PlaceIndicator>
             )}
             <ImageContainer>
-                {infoArr?.map((entry) => {
-                    const imgSrc = entry?.img
-                        ? `${process.env.REACT_APP_BASE_URL}${entry.img}`
-                        : `${process.env.PUBLIC_URL}/loader.png`;
+                {infoArr.length > 0
+                    ? infoArr?.map((entry) => {
+                          const imgSrc = entry?.img
+                              ? `${process.env.REACT_APP_BASE_URL}${entry.img}`
+                              : `${process.env.PUBLIC_URL}/loader.png`;
 
-                    const overlaySrc = entry?.overlay
-                        ? `${process.env.REACT_APP_BASE_URL}${entry.overlay}`
-                        : '';
+                          const overlaySrc = entry?.overlay
+                              ? `${process.env.REACT_APP_BASE_URL}${entry.overlay}`
+                              : '';
 
-                    return (
-                        <ImageWrapper
-                            key={`${entry?.img}.2`}
-                            $width={width}
-                            $height={height}
-                            $translate={translate}
-                            $topLeftRad={topLeftRad}
-                            $src={imgSrc}
-                            $overlay={overlaySrc}
-                        >
-                            {entry?.info && (
-                                <ImgInfo
-                                    dangerouslySetInnerHTML={{
-                                        __html: entry?.info,
-                                    }}
-                                />
-                            )}
-                        </ImageWrapper>
-                    );
-                })}
+                          return (
+                              <ImageWrapper
+                                  key={`${entry?.img}_2`}
+                                  $width={width}
+                                  $height={height}
+                                  $translate={translate}
+                                  $topLeftRad={topLeftRad}
+                                  $src={imgSrc}
+                                  $overlay={overlaySrc}
+                              >
+                                  {entry?.info && (
+                                      <ImgInfo
+                                          dangerouslySetInnerHTML={{
+                                              __html: entry?.info,
+                                          }}
+                                      />
+                                  )}
+                              </ImageWrapper>
+                          );
+                      })
+                    : null}
             </ImageContainer>
             <ButtonWrapper>
                 <StyledButton
