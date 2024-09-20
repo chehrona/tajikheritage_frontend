3c6ff9d4b0f10c46ba7d6311e6f595c30c278cb3
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2024-09-20 12:25:09 -0400

    reverted hash routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <HashRouter>
+    <BrowserRouter>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </HashRouter>,
+    </BrowserRouter>,
 );
