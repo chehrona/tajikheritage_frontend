545e654dca1ade6e9a6bbd653e8d1b4472f333a9
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2024-11-24 22:31:17 -0500

    Update AppLayout.tsx

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -49,13 +49,13 @@ const AppLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
     };
 
     const handleCopy = async () => {
-        // try {
-        //     const textToCopy =
-        //         'The text is copyright protected by thetajikheritage.com.';
-        //     await navigator.clipboard.writeText(textToCopy);
-        // } catch (error) {
-        //     console.error('Unable to copy text to clipboard:', error);
-        // }
+        try {
+            const textToCopy =
+                'The text is copyright protected by thetajikheritage.com.';
+            await navigator.clipboard.writeText(textToCopy);
+        } catch (error) {
+            console.error('Unable to copy text to clipboard:', error);
+        }
     };
 
     return (
